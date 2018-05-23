02cebfe672a8a0933cd29201ca536781af3bd9df
Mantvydas <31690342+mbdeveloping@users.noreply.github.com>
2018-05-23 17:28:12 +0100

    Update
    Making script more simple,
    clearing the code

--- Update v2/script/js.js
+++ Update v2/script/js.js
@@ -1,36 +1,50 @@
 $(document).ready(function() {
   /***** WEDDING PAGE *****/
-
-  /* Main cards rules */
   (function() {
-    let $mainCards = $(".cards");
+    let pageBody = $("body");
+    /* Main cards rules */
+    (function() {
+      let thumbnails = $(".thumbnail");
+      let cards = $(".main-card");
 
-    $mainCards.on('click', function() {
-      let $element = $(this);
+      thumbnails.on('click', function() {
+        let element = $(this);
+        let elementIndex = thumbnails.index(element);
 
-      if ($element.hasClass("royal-card")) {
-        $("body").addClass("body-scroll-lock");
+        pageBody.addClass("body-scroll-lock");
         $(".cards-overlay").addClass("cards-overlay-display").hide().fadeIn('slow');
-        $(".royal-main-card").addClass("cards-overlay-display").hide().fadeIn('slow');
-      }
-      console.log($element);
-    })
+        $(cards[elementIndex]).removeClass("main-card-display");
+      })
+    }());
+
+    /* Cards close button rules */
+    (function() {
+      let closeBtn = $("#card-close-button");
+
+      closeBtn.on('click', function(element) {
+        element.preventDefault();
+        $(".cards-overlay").fadeOut('slow').removeClass("cards-overlay-display");
+        pageBody.removeClass("body-scroll-lock");
+        $(".main-card").addClass("main-card-display");
+      })
+    }());
+
+    /* Footer rules */
+    (function(){
+      let weChatBtn = $("#wechat-overlay-btn a");
+      let weChatClose = $("#weChat-overlay-close");
+
+      weChatBtn.on('click', function(e) {
+        e.preventDefault();
+        pageBody.addClass("body-scroll-lock");
+        $(".weChat-overlay").css("display", "flex").hide().fadeIn();
+      });
+
+      weChatClose.on('click', function(e) {
+        e.preventDefault();
+        pageBody.removeClass("body-scroll-lock");
+        $(".weChat-overlay").fadeOut();
+      })
+    }())
   }());
-  /* Footer rules */
-  (function(){
-    let $weChatBtn = $("#wechat-overlay-btn a");
-    let $weChatClose = $("#weChat-overlay-close");
-
-    $weChatBtn.on('click', function(e) {
-      e.preventDefault();
-      $("body").addClass("body-scroll-lock");
-      $(".weChat-overlay").css("display", "flex").hide().fadeIn();
-    });
-
-    $weChatClose.on('click', function(e) {
-      e.preventDefault();
-      $("body").removeClass("body-scroll-lock");
-      $(".weChat-overlay").fadeOut();
-    })
-  }())
 })
